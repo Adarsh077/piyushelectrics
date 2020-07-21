918ae23b91f6ee9f466a08c1b7d94a8004b4a742
Adarsh077 <adarshco077@gmail.com>
2020-07-21 17:54:00 +0530

    api baseurl changed

--- client/src/services/Axios.js
+++ client/src/services/Axios.js
@@ -1,6 +1,6 @@
 import Axios from "axios";
 
-Axios.defaults.baseURL = "http://localhost:8000";
+Axios.defaults.baseURL = "https://piyushelectrics.herokuapp.com";
 
 // DO NOT MODIFY!! This code returns the res.data in .then()
 Axios.interceptors.response.use(function (response) {
